02e316bd224584c2de1330e811e6711206330590
Anshul-Singla <asingla2807@gmail.com>
2023-02-23 04:27:19 +0530

    yarn + typescript + react for the first time

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>life_is_awesome....</h1>
+    </div>
+  );
+}
+
+export default App;
