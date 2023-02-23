7dbcadd4cfef4078a2371218b4125fd0964f94ed
Anshul-Singla <asingla2807@gmail.com>
2023-02-23 17:37:33 +0530

    db.json added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import './App.css';
 import ReactECharts from 'echarts-for-react';
+import data from './db.json'
 
 function App() {
-  const options  = {
-    grid: { top: 8, right: 8, bottom: 24, left: 36 },
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-        smooth: true,
-      },
-    ],
-    tooltip: {
-      trigger: 'axis',
-    },
-  };
+  console.log('data:', data)
 
   return (
     <div className="App">
       <h1>life_is_awesome....</h1>
-      <ReactECharts option={options} />
     </div>
   );
 }
