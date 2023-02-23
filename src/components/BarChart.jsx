6574e88876811b9e4ed054b44f95e2ecdb032cc9
Anshul-Singla <asingla2807@gmail.com>
2023-02-23 20:21:11 +0530

    bar chart addedf

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactECharts from 'echarts-for-react';
+
+
+const BarChart = ({data}) => {
+    console.log('data:', data);
+    let malic = 0 , count =0 ;
+    const barData = [];
+    for(let i=0 ; i<data.length ; i++){
+        if( i!=data.length-1 && data[i]["Alcohol"] === data[i+1]["Alcohol"]){
+            malic += data[i]["Malic Acid"];
+            count++
+        }else if(i === data.length-1){
+            malic +=data[i]["Malic Acid"];
+            barData.push(malic / count);
+            malic = 0;count=0
+        }
+        else{
+            malic +=data[i+1]["Malic Acid"];
+            barData.push(malic / count);
+            malic = 0;count=0
+        }
+        console.log('barData:', barData)
+    }
+
+    console.log('barData:', barData)
+    const options = {
+        // grid: { top: 8, right: 8, bottom: 24, left: 36 },
+        xAxis: {
+          name :"Alcohol",
+          type:"category",
+          data:['Alcohol1', 'Alcohol2', 'Alcohol3'],
+        },
+        yAxis: {
+          name :"Malic Acid",
+          type: 'value',
+        },
+    
+        series: [
+          {
+            data: barData,
+            type: 'bar',
+            smooth: true,
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+            color: 'rgba(180, 180, 180, 0.2)'
+      }
+          },
+        ],
+        tooltip: {
+          trigger: 'axis',
+        },
+      }
+    
+      return <ReactECharts 
+      option={options} 
+      style={{
+        width:'50vw'
+      }}
+      />
+}
+
+export default BarChart
